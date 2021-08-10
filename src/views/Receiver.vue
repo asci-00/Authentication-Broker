5a94d72e93309a542652685a36701431baf0b5a0
me <20161564@vision.hoseo.edu>
2021-08-10 12:45:54 +0900

    receiver test

--- src/views/Receiver.vue
+++ src/views/Receiver.vue
@@ -3,11 +3,14 @@
 </template>
 
 <script>
+import { setToken } from '@/apis/Api'
+
 export default {
   props : ['token'],
   created() {
     if(this.token) {
       sessionStorage.setItem('token', this.token)
+      setToken(this.token)
       this.$router.push('/')
     } else this.$router.push('/notfound')
   }
