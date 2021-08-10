29678c2533ad41e016511361b03974890e79e04b
cryingnavi <cryingnavi@iochord.com>
2021-08-10 14:14:43 +0900

    router 수정

--- src/router/index.js
+++ src/router/index.js
@@ -39,7 +39,7 @@ export const routes = [
 
 const router = new VueRouter({
   mode: 'history',
-  base: process.env.BASE_URL,
+  base: '/brokerAdmin/',
   routes
 })
 
--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,5 @@
 module.exports = {
     devServer: {
-        host:'localhost',
         port: 3201,
-        public: 'http://0.0.0.0:3201'
-    }
+    },
 }
