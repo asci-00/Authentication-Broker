0330d6bf46ecec1ad174d150864dd96cf7bae57b
cryingnavi <cryingnavi@iochord.com>
2021-10-01 19:52:40 +0900

    Fix: host name이 적용되지 않는 현상

--- src/modules/static/dataTransform.js
+++ src/modules/static/dataTransform.js
@@ -47,7 +47,8 @@ export const objectToTree = (obj) => {
 //policy create function
 export const getRequestParam = (obj) => {
     const host = obj.required_info[2]
-    const host_name = host === '' ? 'default' : host.value
+    const host_name = (host && host.value !== '') ? host.value : 'default'
+	console.warn(host, host.value, host_name, host.value !== '')
     const path = (
         obj.type === 'internal' ? 
             obj.required_info[1]['value'] : 
@@ -60,6 +61,7 @@ export const getRequestParam = (obj) => {
         if(item['key'] == 'none' || item['value'] == '') return
         data[item['key']] = item['value']
     })
+	console.warn(obj.type, path, data)
     return { type : obj.type, path, data : { data } }
 }
 
