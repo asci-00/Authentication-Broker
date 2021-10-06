b1f995d9ffa4f45b04e4b1a7124306b036438ea6
cryingnavi <cryingnavi@iochord.com>
2021-10-06 09:15:00 +0900

    Dev: 요구사항변경 - API Protocol ID / PW 추가

--- src/modules/static/common.js
+++ src/modules/static/common.js
@@ -1,7 +1,7 @@
 //export const connect_keys = ['ID', 'Password', 'Session_Key', 'Token', 'Api_Key', 'Port']
 export const connect_keys = {
     ssh : ['ID', 'Password', 'Port'],
-    api : ['Api_Key', 'Port'],
+    api : ['Api_Key', 'Port', 'ID', 'Password'],
     hmac : ['Session_Key', 'Token', 'Port'],
     gui : ['ID', 'Password', 'Port']
 }
